b420a417c0c4a1fa9a39a7324054f496333bb00d
mustafor907-byte <mustafor907@gmail.com>
2026-07-29 16:28:11 +0300

    m

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    // Scroll hodisasini tinglash
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = 'block'; // Tugmani ko'rsatish
+        } else {
+            backToTopButton.style.display = 'none'; // Tugmani yashirish
+        }
+    });
+
+    // Tugma bosilganda yuqoriga qaytish
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
